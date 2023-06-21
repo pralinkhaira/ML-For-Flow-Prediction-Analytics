8137bae18115edd8b6cc41b4a32c3ecada8434ad
BitH0xer <132118661+PralinKhaira@users.noreply.github.com>
2023-06-21 14:45:06 +0530

    Create Flow_Output(Data Loaded And Preprocessed).py

---
+++ Flow_Output(Data Loaded And Preprocessed).py
@@ -0,0 +1,25 @@
+# This code assumes that you have already loaded and preprocessed your data
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+# Assuming you have loaded and preprocessed your data into X (input features) and y (output labels)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create and train a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
+print("Mean Absolute Error:", mae)
